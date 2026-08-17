eb1807c8a34ca1d3071aaf6864cd91c2b20bfef3
Kristóf Mészáros <meszikristof@live.com>
2026-08-17 21:44:24 +0200

    Revert to GitHub Pages and setup OAuth backend

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.kolumbia17.hu',
+  site: 'https://meszikristof.github.io',
+  base: '/kolumbia17',
 });
