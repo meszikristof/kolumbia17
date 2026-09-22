87badcff646708044a662de1db222eb1b2b1b1ec
Kristóf Mészáros <meszikristof@live.com>
2026-09-22 22:20:30 +0200

    Use Google Docs viewer for PDF previews to fix Firefox embed issue

--- src/utils/remark-document-embed.js
+++ src/utils/remark-document-embed.js
@@ -42,7 +42,7 @@ function buildEmbedHtml(src, title, siteUrl) {
 
   let iframeSrc = '';
   if (docType === 'pdf') {
-    iframeSrc = absoluteSrc;
+    iframeSrc = `https://docs.google.com/viewer?url=${encodeURIComponent(absoluteSrc)}&embedded=true`;
   } else if (docType !== 'document') {
     iframeSrc = `https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(absoluteSrc)}`;
   }
